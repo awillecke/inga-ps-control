8b04e4596a9fedacdb89b502515f26867485eb22
goldste1n <goldstein@springer-laptop.(none)>
2013-04-29 23:37:13 +0200

    empfaenger fuer boxes

--- gateway/gateway.c
+++ gateway/gateway.c
@@ -1,1 +1,36 @@
-//1
+#include "contiki.h"
+#include "net/rime.h"
+#include "random.h"
+
+#include "dev/button-sensor.h"
+
+#include "dev/leds.h"
+
+#include <stdio.h>
+/*---------------------------------------------------------------------------*/
+PROCESS(example_broadcast_process, "Broadcast example");
+AUTOSTART_PROCESSES(&example_broadcast_process);
+/*---------------------------------------------------------------------------*/
+static void broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from) {
+    printf("broadcast message received from %d.%d: '%s'\n",
+         from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+}
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static struct broadcast_conn broadcast;
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(example_broadcast_process, ev, data) {
+    static struct etimer et;
+
+    PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+
+    PROCESS_BEGIN();
+
+    broadcast_open(&broadcast, 129, &broadcast_call);
+
+    while(1) {
+        PROCESS_YIELD();
+    }
+  
+    PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
