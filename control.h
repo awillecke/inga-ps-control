c4537564bb46266892bb844301b5664e6dfaef7a
z4nder <alexander.willecke@gmx.de>
2013-04-29 22:05:52 +0200

    add control.h

---
+++ control.h
@@ -0,0 +1,18 @@
+#include "contiki.h"
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "acc-sensor.h"
+#include "gyro-sensor.h"
+#include "pressure-sensor.h"
+#include <stdio.h>
+#include "net/rime.h"
+
+
+//Buttons
+#define CROSS 0
+#define CIRCLE 1
+#define TRIANGLE 2
+#define SQUARE 3
+
+
