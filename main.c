ddca2e7391024c6306aaeed5bcfbee3fcf28e757
z4nder <alexander.willecke@gmx.de>
2013-04-29 20:40:18 +0200

    first threshold

--- main.c
+++ main.c
@@ -15,9 +15,12 @@ PROCESS(default_app_process, "Hello world process");
 AUTOSTART_PROCESSES(&default_app_process);
  
 static struct etimer timer;
+static int16_t x = 0;
+static int count = 0;
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(default_app_process, ev, data) {
+
   PROCESS_BEGIN();
 
   SENSORS_ACTIVATE(acc_sensor);
@@ -28,11 +31,15 @@ PROCESS_THREAD(default_app_process, ev, data) {
   while (1) { 
     PROCESS_YIELD();
     if ( etimer_expired(&timer) ) {
-       etimer_set(&timer, CLOCK_SECOND*0.5);
+       etimer_set(&timer, CLOCK_SECOND*0.05);
 
-       printf("Lage: X: %d, Y: %d, Z: %d. ",gyro_sensor.value(GYRO_X_RAW), gyro_sensor.value(GYRO_Y_RAW), gyro_sensor.value(GYRO_Z_RAW));
-       printf("Beschl.: X: %d, Y: %d, Z: %d.\n",acc_sensor.value(ACC_X), acc_sensor.value(ACC_Y), acc_sensor.value(ACC_Z));
-
+//       printf("Lage: X: %4d, Y: %4d, Z: %4d. ",gyro_sensor.value(GYRO_X_RAW), gyro_sensor.value(GYRO_Y_RAW), gyro_sensor.value(GYRO_Z_RAW));
+//       printf("Beschl.: X: %6d, Y: %6d, Z: %6d.\n",acc_sensor.value(ACC_X_RAW), acc_sensor.value(ACC_Y_RAW), acc_sensor.value(ACC_Z_RAW));
+         x = acc_sensor.value(ACC_X_RAW);
+         if (x > 400) {
+           printf("BOX!!! nr %d over 400\n", count);
+           count++;
+         }
        }
   }
   PROCESS_END();
