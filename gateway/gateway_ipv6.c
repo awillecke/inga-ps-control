79bb25df65fd55a69c673c8eddd9373fc218f8d6
z4nder <alexander.willecke@gmx.de>
2013-05-29 16:23:00 +0200

    alles

--- gateway/gateway_ipv6.c
+++ gateway/gateway_ipv6.c
@@ -2,6 +2,8 @@
 /*---------------------------------------------------------------------------*/
 PROCESS(gateway, "gateway main processs");
 AUTOSTART_PROCESSES(&gateway);
+
+static uint16_t port = 0b1111111111111111;
 /*---------------------------------------------------------------------------*/
 static struct uip_udp_conn *server_conn;
 static int recv;
@@ -17,7 +19,6 @@ static void udp_receive_data(void) {
 static void print_local_addresses(void) {
     int i;
     uint8_t state;
-
     PRINTF("Server IPv6 addresses: ");
     for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
         state = uip_ds6_if.addr_list[i].state;
@@ -29,13 +30,15 @@ static void print_local_addresses(void) {
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(gateway, ev, data) {
-     
-    PORTA = 0b00000011;
-    DDRA = 0b00000011;
   
     PROCESS_BEGIN();
 
-
+    i2c_init();
+    
+    set_button(0);
+    set_button(0);
+    
+    
     print_local_addresses();
 
     server_conn = udp_new(NULL, UIP_HTONS(3003), NULL);
@@ -43,29 +46,62 @@ PROCESS_THREAD(gateway, ev, data) {
 
     while(1) {
         PROCESS_YIELD();
-        //printf("back\n");
         if(ev == tcpip_event) {
-            //printf("tcp event\n");
             udp_receive_data();
-
-            if (recv == 2) {  //TRI
-                PORTA &= ~(1<<PA0);
-                //PORTA |= (1<<PA0);
-                printf("SETTING IO\n");
-                etimer_set(&timer, CLOCK_SECOND*0.01);//0.05);
+            if (recv == CROSS) {  //CROSS
+                set_button(CROSS);
+                etimer_set(&timer, CLOCK_SECOND*BUTTON_HOLD);
                 PROCESS_YIELD();
-                printf("UNSET IO\n");
-                PORTA |= (1<<PA0);
-                //PORTA &= ~(1<<PA0);
+                set_button(CROSS);
+                recv = 0;
+            }
+            else if (recv == CIRCLE) {  //CIRCLE
+                set_button(CIRCLE);
+                etimer_set(&timer, CLOCK_SECOND*BUTTON_HOLD);
+                PROCESS_YIELD();
+                set_button(CIRCLE);
+                recv = 0;
+            }
+            else if (recv == TRIANGLE) {  //TRI
+                set_button(TRIANGLE);
+                etimer_set(&timer, CLOCK_SECOND*BUTTON_HOLD);
+                PROCESS_YIELD();
+                set_button(TRIANGLE);
                 recv = 0;
             } 
-            else if (recv == 3) {  //SQUARE
-                PORTA &= ~(1<<PA1);   
-                printf("SETTING IO\n");
-                etimer_set(&timer, CLOCK_SECOND*0.01);//0.05);
+            else if (recv == SQUARE) {  //SQUARE
+                set_button(SQUARE);
+                etimer_set(&timer, CLOCK_SECOND*BUTTON_HOLD);
                 PROCESS_YIELD();
-                printf("UNSET IO\n");
-                PORTA |= (1<<PA1);  
+                set_button(SQUARE);
+                recv = 0;
+            }
+            else if (recv == UP) {  //UP
+                set_button(UP);
+                etimer_set(&timer, CLOCK_SECOND*BUTTON_HOLD);
+                PROCESS_YIELD();
+                set_button(UP);
+                recv = 0;
+            }
+            else if (recv == DOWN) {  //DOWN
+                set_button(DOWN);
+                etimer_set(&timer, CLOCK_SECOND*BUTTON_HOLD);
+                PROCESS_YIELD();
+                set_button(DOWN);
+                recv = 0;
+            }
+            else if (recv == LEFT) {  //LEFT
+                set_button(LEFT);
+                etimer_set(&timer, CLOCK_SECOND*BUTTON_HOLD);
+                PROCESS_YIELD();
+                set_button(LEFT);
+                recv = 0;
+            }
+            else if (recv == RIGHT) {  //RIGHT
+                set_button(RIGHT);
+                etimer_set(&timer, CLOCK_SECOND*BUTTON_HOLD);
+                PROCESS_YIELD();
+                set_button(RIGHT);
                 recv = 0;
             }      
          }  
@@ -73,19 +109,26 @@ PROCESS_THREAD(gateway, ev, data) {
     PROCESS_END();
 }
 /*--------------------------------------------------------------------------------------*/
-int set_button(uint16_t output)
+int set_button(uint8_t output)
 {
-    uint8_t ret,            
-            ic1 = (uint8_t)output,
-            ic2 = (uint8_t)(output >> 8);
+    uint16_t tmp = 0;
     
+    tmp = (1 << output);
+  //  printf("tmp:  %d\n", tmp);
+    port ^= tmp;
+//    printf("port: %d\n", port);
+    
+    uint8_t ret = -1,            
+            ic1 = (uint8_t)port,
+            ic2 = (uint8_t)(port >> 8);   
+            
     printf("ic1: %d, ic2: %d\n", ic1, ic2);
    
-    i2c_init();
+    //i2c_init();
 
-    if(i2c_start(IC1_ADDR_W))
+    if(i2c_start(IC1_ADDR_W) == 0)
     {
-        ret = i2c_write(output);
+        ret = i2c_write(ic1);
     }
     else
     {
@@ -93,16 +136,18 @@ int set_button(uint16_t output)
     }
 
     i2c_stop();
-    
-    if(i2c_start(IC2_ADDR_W))
+
+    if(i2c_start(IC2_ADDR_W) == 0)
     {
-        ret = i2c_write(output);
+        ret = i2c_write(ic2);
     }
     else
     {
         printf("i2c_start failed: IC2\n");
     }    
-  
-  return ret;     
+    
+    i2c_stop();
+    
+    return ret;     
 }
 /*--------------------------------------------------------------------------------------*/
