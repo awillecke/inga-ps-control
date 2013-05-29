e461df059cb053ee9482a494b6b4d26b2471cfbf
z4nder <alexander.willecke@gmx.de>
2013-05-29 22:56:32 +0200

    bugs fixed

--- gateway/gateway.c
+++ gateway/gateway.c
@@ -3,6 +3,9 @@
 PROCESS(gateway, "gateway main processs");
 AUTOSTART_PROCESSES(&gateway);
 /*---------------------------------------------------------------------------*/
+static uint8_t bit_mask_buttons = 0b11111111;
+static uint8_t bit_mask_movement = 0b11111111;
+
 static struct uip_udp_conn *server_conn;
 static int recv;
 static struct etimer timer;
@@ -10,8 +13,7 @@ static struct etimer timer;
 static void udp_receive_data(void) {
     if(uip_newdata()) {
         recv = atoi((char *)uip_appdata);
-        printf("Empfange Packet: %d\n", recv);      
-        set_control(recv); 
+        printf("Empfange Packet: %d\n", recv);
     }
 }
 
@@ -47,6 +49,12 @@ PROCESS_THREAD(gateway, ev, data) {
         PROCESS_YIELD();
         if(ev == tcpip_event) {
             udp_receive_data();
+            
+            set_control(recv); 
+            etimer_set(&timer, CLOCK_SECOND*BUTTON_HOLD);
+            PROCESS_YIELD();
+            set_control(recv);
+            recv = 0;
          }  
     }  
     PROCESS_END();
@@ -57,8 +65,10 @@ int set_control(uint8_t output) {
     uint8_t ret = -1;
 
     if (((output & 0b10000000) >> MOVE_CONTROL) == 0) {
+        bit_mask_buttons ^= output;
+        
         if(i2c_start(IC1_ADDR_W) == 0) {
-            ret = i2c_write(output);
+            ret = i2c_write(bit_mask_buttons);
             i2c_stop();
         } 
         else {
@@ -66,8 +76,10 @@ int set_control(uint8_t output) {
         }
     }
     else {
+        bit_mask_movement ^= output;
+        
         if(i2c_start(IC2_ADDR_W) == 0) {
-            ret = i2c_write(output);
+            ret = i2c_write(bit_mask_movement);
             i2c_stop();
         } 
         else {
