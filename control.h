e630f65c19fdb6f3c22ff78aa367fa34244a6a98
z4nder <alexander.willecke@gmx.de>
2013-05-29 21:56:22 +0200

    new defines for buttons

--- control.h
+++ control.h
@@ -1,12 +1,18 @@
 #include "contiki.h"
+#include "contiki-net.h"
+#include "settings.h"
+#include "node-id.h"
 #include <util/delay.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "acc-sensor.h"
 #include "gyro-sensor.h"
 #include "pressure-sensor.h"
-#include <stdio.h>
-#include "net/rime.h"
+#include <i2c.h>
+
+#include "net/uip-debug.h"
+#define DEBUG DEBUG_PRINT
+#define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
 
 
 //Buttons
@@ -14,5 +20,23 @@
 #define CIRCLE 1
 #define TRIANGLE 2
 #define SQUARE 3
+#define START 4
+#define SELECT 5
+#define BUTTON_6 6
+#define MOVE_CONTROL 7
+ 
+
+//Movement
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+#define L1 4
+#define L2 5
+#define R1 6
+#define R2 7
 
 
+//                 fest A  R/W
+//                 |--||-|  |
+#define IC1_ADDR_R 0b01000001
